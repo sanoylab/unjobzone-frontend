e5473957bceaa1d7f8aa189e147838cc693732e8
yonas <expertsanoy@gmail.com>
2024-11-23 18:47:12 -0700

    update on the components

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
 
-dotenv.config();
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `${process.env.VITE_API_URI}`,
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, '')
-      }
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URI, // Use the loaded environment variable
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+        },
+      },
     },
-  },
-})
+  };
+});
