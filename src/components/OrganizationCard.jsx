dd83dd9de5b012ee0cf050ac5ba0a24d5bc90b32
yonas <expertsanoy@gmail.com>
2024-11-29 23:05:53 -0700

    add footer on the homepage

--- src/components/OrganizationCard.jsx
+++ src/components/OrganizationCard.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom'
 const OrganizationCard = ({image, title, totalJobs}) => {
   return (
 
-<div className="col-xl-4 col-lg-4 col-md-6" >
-      <div class="rts__job__card rounded-2 style__four" style={{ height: '390px' }}>
-        <div class="mx-auto rounded-0">
-            <img src={image} alt="" style={{height: '130px'}} />
+    <div class="col-xl-4 col-lg-4 col-md-6">
+    <div class="rts__job__card rounded-2 style__four">
+        <div class="company__icon mx-auto rounded-0">
+            <img src={image} alt="" />
         </div>
         <div class="h6 job__title mt-3 mb-2">
             <Link to={`/jobs?dept=${encodeURIComponent(title)}`} aria-label="job">
@@ -15,7 +15,7 @@ const OrganizationCard = ({image, title, totalJobs}) => {
         </div>
        
         
-        <Link to={`/jobs?dept=${encodeURIComponent(title)}`} class="rts__btn fill__btn rounded-2">{totalJobs} open jobs </Link>
+        <Link to={`/jobs?dept=${encodeURIComponent(title)}`} class="rts__btn fill__btn rounded-2">open Job {totalJobs}</Link>
     </div>
 </div>
 
