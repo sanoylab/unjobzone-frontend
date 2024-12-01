f8d7f7c816968b8b8688b0d527d111fc4033a96d
yonas <expertsanoy@gmail.com>
2024-11-30 23:45:36 -0700

    clean up the pages

--- src/components/DutyStationCard.jsx
+++ src/components/DutyStationCard.jsx
@@ -6,7 +6,7 @@ const DutyStationCard = ({title, totalJobs}) => {
 
     <div className="col-lg-3" >
                             <div className="rts__job__card__big style__gradient__two 
-                            flex-wrap d-flex justify-content-between align-items-center gap-3" style={{minHeight: "150px"}}>
+                            flex-wrap d-flex justify-content-between align-items-center gap-3" style={{minHeight: "180px"}}>
                                 <div className="d-flex gap-3 flex-column flex-md-row mb-3 mb-md-0 justify-content-start align-items-center">
                                     
                                     <div className="job__meta">
