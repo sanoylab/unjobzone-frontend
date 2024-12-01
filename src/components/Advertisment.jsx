d7afd36193d5935b30b994ee020909bf77574aaa
yonas <expertsanoy@gmail.com>
2024-11-30 23:35:21 -0700

    clean up the pages

--- src/components/Advertisment.jsx
+++ src/components/Advertisment.jsx
@@ -7,31 +7,31 @@ const Advertisment = () => {
             <div className="col-lg-6">
                 <div className="rts__content__section">
                     <h3 className="fw-bold l--3 mb-4  wow animated fadeInUp">1000+ Jobs in UN Job Zone Find The One That Best Match You</h3>
-                    <p className=" wow animated fadeInUp">Whether you're an experienced professional or a fresh graduate eager to dive into the workforce, we have something for everyone. From tech wizards to marketing mavens</p>
+                    <p className=" wow animated fadeInUp">Whether you're an experienced professional or a fresh graduate eager to make a difference, the United Nations offers opportunities for everyone. From humanitarian aid workers to policy analysts, your skills can contribute to global peace and development.</p>
                     <div className="mt-40 rts__listing style__six  wow animated fadeInUp">
                         <div className="single__listing d-flex align-items-center gap-4">
                             <span className="icon">
                                 <i className="fa-regular fa-check"></i> 
                             </span>
-                            <span>Quality Job</span>
+                            <span>Make a global impact</span>
                         </div>
                         <div className="single__listing d-flex align-items-center gap-4">
                             <span className="icon">
                                 <i className="fa-regular fa-check"></i> 
                             </span>
-                            <span>Reach 100s contacts</span>
+                            <span>Work with diverse teams</span>
                         </div>
                         <div className="single__listing d-flex align-items-center gap-4">
                             <span className="icon">
                                 <i className="fa-regular fa-check"></i> 
                             </span>
-                            <span>No Extra Charge</span>
+                            <span>Access to continuous learning</span>
                         </div>
                         <div className="single__listing d-flex align-items-center gap-4">
                             <span className="icon">
                                 <i className="fa-regular fa-check"></i> 
                             </span>
-                            <span>Internation job</span>
+                            <span>Competitive benefits and compensation</span>
                         </div>
                     </div>
                     <a href="contact-2.html" className="rts__btn common__btn he-3 fill__btn mt-50">Read More <i className="fa-regular fa-arrow-right"></i></a>
@@ -47,29 +47,10 @@ const Advertisment = () => {
                             <img src="../assets/img/home-6/about/1.webp" alt="" />
                         </figure>
                     </div>
-                    <div className="about__applicant">
-                        <div className="rts__applicants">
-                            <span className="font-20 mb-3 d-block fw-medium">Applicants</span>
-                            <div className="applicant__list">
-                                <div className="single__list">
-                                    <img src="assets/img/author/1.svg" alt="" />
-                                </div>
-                                <div className="single__list">
-                                    <img src="assets/img/author/2.svg" alt="" />
-                                </div>
-                                <div className="single__list">
-                                    <img src="assets/img/author/3.svg" alt="" />
-                                </div>
-                                <div className="single__list">
-                                    <img src="assets/img/author/4.svg" alt="" />
-                                    <div className="icon-plus"><i className="fa-sharp fa-solid fa-plus"></i></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   
                     <div className="about__image__two align-self-start">
                         <figure>
-                            <img src="../assets/img/home-6/about/2.webp" alt="" />
+                            <img src="../assets/images/UN.jpg" alt="" />
                         </figure>
                     </div>
                 </div>
