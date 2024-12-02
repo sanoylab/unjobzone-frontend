b552201146841ec00f4ce5e34fb80dcb1f474607
yonas <expertsanoy@gmail.com>
2024-12-02 07:04:03 -0700

    add homepage organization slider

--- src/components/OrganizationLogoSlider.jsx
+++ src/components/OrganizationLogoSlider.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getJobOrganization } from "../Api";
+// Update the import path to match where Swiper is located in your project
 import Spinner from "./Spinner";
 
 const OrganizationLogoSlider = () => {
@@ -25,66 +26,65 @@ const OrganizationLogoSlider = () => {
     }
 
     fetchOrganizations();
+
+    const swiper = new Swiper('.swiper-data', {
+      slidesPerView: 7,
+      loop: true,
+      speed: 1000,
+      autoplay: {
+        delay: 7000,
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+        1200: {
+          slidesPerView: 6,
+        },
+        1400: {
+          slidesPerView: 7,
+        },
+      },
+    });
+
+    return () => {
+      swiper.destroy();
+    };
   }, []);
   
   return (
-    <div className="rts__brand  wow animated fadeInUp  ">
-        <div className="container">
-          <div className="bg-white">
-            <div className="row align-items-center">
-              <div
-                className="rts__brand__slider overflow-hidden swiper-data"
-                data-swiper='{
-                            "slidesPerView":7,
-                            "loop": true,
-                            "speed": 1000,
-                            "autoplay":{
-                                "delay":"7000"
-                            },
-            
-                            "breakpoints": {
-                                "0": {
-                                    "slidesPerView": 2
-                                },
-                                "768": {
-                                    "slidesPerView": 3
-                                },
-                                "1024": {
-                                    "slidesPerView": 4
-                                },
-                                "1200": {
-                                    "slidesPerView": 6
-                                },
-                                "1400": {
-                                    "slidesPerView": 7
-                                }
-                            }
-            
-                        }'
-              >
-                <div className="swiper-wrapper">
-
+    <div className="rts__brand wow animated fadeInUp">
+      <div className="container">
+        <div className="bg-white">
+          <div className="row align-items-center">
+            <div className="rts__brand__slider overflow-hidden swiper-data">
+              <div className="swiper-wrapper">
                 {organizations.map((org, index) => (
-                  <div className="swiper-slide" key={index} style={{height: '50px'}}>
+                  <div className="swiper-slide" key={index} style={{ height: '50px' }}>
                     <div className="brand__item">
-
                       <Link to={`/jobs?dept=${encodeURIComponent(org.dept)}`} className="brand__item__link" aria-label="brand">
                         <img
                           src={`${APP_URL}/assets/logo/${org.logo}`}
                           alt=""
-                          style={{height: '50px'}}
+                          style={{ height: '50px' }}
                         />
                       </Link>
                     </div>
                   </div>
                 ))}
-                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-  )
+    </div>
+  );
 }
 
-export default OrganizationLogoSlider
+export default OrganizationLogoSlider;
