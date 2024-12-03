f0b0e4dfb944f8454f54c9f11bef0e20e0d3bfbf
yonas <expertsanoy@gmail.com>
2024-12-03 10:12:01 -0700

    fix the css of the organization page

--- src/components/OrganizationCard.jsx
+++ src/components/OrganizationCard.jsx
@@ -1,23 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
 const OrganizationCard = ({image, title, totalJobs}) => {
   return (
 
-    <div class="col-xl-3 col-lg-3 col-md-6">
-    <div class="rts__job__card rounded-2 style__four" style={{height: '450px'}}>
+    <div class="rts__job__card rounded-2 style__four organization-card" >
         <div class="company__icon mx-auto rounded-0">
             <img src={image} alt="" />
         </div>
-        <div class="h6 job__title mt-3 mb-2">
+        <div class="h6 job__title mt-3 mb-10">
             <Link to={`/jobs?dept=${encodeURIComponent(title)}`} aria-label="job">
             {title}
             </Link>
         </div>
-       
+        <div className="card-footer">
+        <Link to={`/jobs?dept=${encodeURIComponent(title)}`} class="rts__btn fill__btn rounded-2 job-link">View {totalJobs} jobs</Link>
+
+      </div>
         
-        <Link to={`/jobs?dept=${encodeURIComponent(title)}`} class="rts__btn fill__btn rounded-2">open Job {totalJobs}</Link>
     </div>
-</div>
 
 
 
