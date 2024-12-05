427c36a6ccc6be8d9b44f09ec1f8575649f33083
yonas <expertsanoy@gmail.com>
2024-12-04 19:29:46 -0700

    add job categories page and rearrange menu items

--- src/components/JobCategory.jsx
+++ src/components/JobCategory.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-const JobCategory = ({image, title, totalJobs}) => {
+const JobCategory = ({image, title, link, totalJobs}) => {
   return (
     <div className="col-xl-4 col-lg-6 col-md-6">
     <div className="single__cat style__six align-items-center d-flex gap-4">
@@ -12,7 +12,7 @@ const JobCategory = ({image, title, totalJobs}) => {
         />
       </div>
       <div className="single__cat__link d-flex w-100 justify-content-between flex-wrap">
-      <Link to={`/jobs?jn=${encodeURIComponent(title)}`} aria-label="cat__label">
+      <Link to={link} aria-label="cat__label">
           {title}
         </Link>
         <span>{totalJobs}+ Jobs</span>
