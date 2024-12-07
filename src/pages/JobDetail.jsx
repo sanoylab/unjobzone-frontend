181fd3f432c4768872a84b9a7c2021b7a53b9333
yonas <expertsanoy@gmail.com>
2024-12-06 20:59:33 -0700

    linkedin login

--- src/pages/JobDetail.jsx
+++ src/pages/JobDetail.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import { getJobDetail } from '../Api';
+import React, { useEffect, useState } from "react";
+import { getJobDetail } from "../Api";
 import JobDetailHero from "../components/JobDetailHero";
-import Spinner from '../components/Spinner';
-import { useParams } from 'react-router-dom';
-import { marked } from 'marked';
+import Spinner from "../components/Spinner";
+import { useParams } from "react-router-dom";
+import { marked } from "marked";
 
 function JobDetail() {
   const { id } = useParams();
@@ -36,114 +36,124 @@ function JobDetail() {
 
   return (
     <div>
-<JobDetailHero 
-  job = {job}
-
-/>
+      <JobDetailHero job={job} />
 
       <div className="rts__section section__padding">
         <div className="container">
-            <div className="row g-30">
-               <div className="col-lg-7 col-xl-8">
-                <div className="rts__job__details">
-                   
-                    <div id="description" className="mb-30">
-                        <h6 className="fw-semibold mb-20">Job Description</h6>
-                        <div dangerouslySetInnerHTML={{ __html: marked(job.job_description) }} />                    </div>
-                   
-                  
-                    
-                  
-                    <div className="d-flex flex-wrap gap-4 mt-40 mb-60">
-                    <a href={job.apply_link} target="_blank" class="rts__btn apply__btn fw-bold">Apply This Position</a>
-                        <div className="d-flex gap-3 align-items-center">
-                            <span className="h6 fw-semibold">Share</span>
-                            <div className="rts__social d-flex gap-3">
-    <a href="https://facebook.com"  aria-label="facebook">
-        <i className="fa-brands fa-facebook"></i>
-    </a>
-    <a href="https://instagram.com"  aria-label="instagram">
-        <i className="fa-brands fa-instagram"></i>
-    </a>
-    <a href="https://linkedin.com"  aria-label="linkedin">
-        <i className="fa-brands fa-linkedin"></i>
-    </a>
-    <a href="https://pinterest.com"  aria-label="pinterest">
-        <i className="fa-brands fa-pinterest"></i>
-    </a>
-    <a href="https://youtube.com"  aria-label="youtube">
-        <i className="fa-brands fa-youtube"></i>
-    </a>
-</div>
-                        </div>
-                    </div>
+          <div className="row g-30">
+            <div className="col-lg-7 col-xl-8">
+              <div className="rts__job__details">
+                <div id="description" className="mb-30">
+                  <h6 className="fw-semibold mb-20">Job Description</h6>
+                  <div
+                    dangerouslySetInnerHTML={{
+                      __html: marked(job.job_description),
+                    }}
+                  />{" "}
                 </div>
-            
-               </div>
-               <div className="col-lg-5 col-xl-4 d-flex flex-column gap-40">
-                   <div class="company__card">                        
-                        <h5 class="company__name mt-20">{job.dept}</h5>
-                        <a href={job.apply_link} target="_blank" class="rts__btn apply__btn mt-40">Apply This Position</a>
-                       
 
+                <div className="d-flex flex-wrap gap-4 mt-40 mb-60">
+                  <a
+                    href={job.apply_link}
+                    target="_blank"
+                    class="rts__btn apply__btn fw-bold"
+                  >
+                    Apply This Position
+                  </a>
+                  <div className="d-flex gap-3 align-items-center">
+                    <span className="h6 fw-semibold">Share</span>
+                    <div className="rts__social d-flex gap-3">
+                      <a href="https://facebook.com" aria-label="facebook">
+                        <i className="fa-brands fa-facebook"></i>
+                      </a>
+                      <a href="https://instagram.com" aria-label="instagram">
+                        <i className="fa-brands fa-instagram"></i>
+                      </a>
+                      <a href="https://linkedin.com" aria-label="linkedin">
+                        <i className="fa-brands fa-linkedin"></i>
+                      </a>
+                      <a href="https://pinterest.com" aria-label="pinterest">
+                        <i className="fa-brands fa-pinterest"></i>
+                      </a>
+                      <a href="https://youtube.com" aria-label="youtube">
+                        <i className="fa-brands fa-youtube"></i>
+                      </a>
                     </div>
-                    <div class="company__card">                        
-                    <p>
-      Sign in with your LinkedIn account <i className="fab fa-linkedin"></i> to get an automatically generated cover letter for this job.
-    </p>                                         
-     <a href="#"  class="rts__btn apply__btn mt-20">Get a Cover Letter</a>
-
-                    </div>
-                    <div className="job__overview">
-                        <h6 className="fw-semibold mb-20">Job Overview</h6>
-                        <div className="job__overview__content">
-                        <ul>
-                   
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-5 col-xl-4 d-flex flex-column gap-40">
+              <div class="company__card">
+                <h5 class="company__name mt-20">{job.dept}</h5>
+                <a
+                  href={job.apply_link}
+                  target="_blank"
+                  class="rts__btn apply__btn mt-40"
+                >
+                  Apply This Position
+                </a>
+              </div>
+              {/* <div class="company__card">
+                <p>
+                  Sign in with your LinkedIn account{" "}
+                  <i className="fab fa-linkedin"></i> to get an automatically
+                  generated cover letter for this job.
+                </p>
+                <a href="#" class="rts__btn apply__btn mt-20">
+                  Get a Cover Letter
+                </a>
+              </div> */}
+              <div className="job__overview">
+                <h6 className="fw-semibold mb-20">Job Overview</h6>
+                <div className="job__overview__content">
+                  <ul>
                     <li className="d-flex flex-wrap gap-0 gap-sm-0 ">
-                      <span className="left-text">  Type</span>
-                      <span className="left-text">: {job.recruitment_type}</span>
+                      <span className="left-text"> Type</span>
+                      <span className="left-text">
+                        : {job.recruitment_type}
+                      </span>
                     </li>
                     <li className="d-flex flex-wrap gap-3 gap-sm-0 ">
-                      <span className="left-text">  Job Posted</span>
-                      <span className="left-text">: {new Date(job.start_date).toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' })}</span>
+                      <span className="left-text"> Job Posted</span>
+                      <span className="left-text">
+                        :{" "}
+                        {new Date(job.start_date).toLocaleDateString("en-US", {
+                          month: "short",
+                          day: "2-digit",
+                          year: "numeric",
+                        })}
+                      </span>
                     </li>
                     <li className="d-flex flex-wrap gap-3 gap-sm-0 ">
-                      <span className="left-text">  Job Deadline</span>
-                      <span className="left-text">: {new Date(job.end_date).toLocaleDateString('en-US', { month: 'short', day: '2-digit', year: 'numeric' })}</span>
+                      <span className="left-text"> Job Deadline</span>
+                      <span className="left-text">
+                        :{" "}
+                        {new Date(job.end_date).toLocaleDateString("en-US", {
+                          month: "short",
+                          day: "2-digit",
+                          year: "numeric",
+                        })}
+                      </span>
                     </li>
                     <li className="d-flex flex-wrap gap-1 gap-sm-0 ">
-                      <span className="left-text">  Job Category</span>
+                      <span className="left-text"> Job Category</span>
                       <span className="left-text">: {job.category_code}</span>
                     </li>
                     <li className="d-flex flex-wrap gap-3 gap-sm-0 ">
                       <span className="left-text"> {job.duty_station}</span>
                     </li>
                     <li className="d-flex flex-wrap gap-3 gap-sm-0 ">
-                      <span className="left-text">  Language: &nbsp;</span>
+                      <span className="left-text"> Language: &nbsp;</span>
                       <span className="left-text">{job.language}</span>
                     </li>
                   </ul>
-                        </div>
-                    </div>
-                  
-                    
-                   
-               </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-     </div>
-
-
-
-
-
-
-
-
-
-
-
-   
+      </div>
     </div>
   );
 }
