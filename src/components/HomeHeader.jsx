ebf010a7d4437b64a174bff745e2b4f72e305d80
yonas <expertsanoy@gmail.com>
2024-12-10 10:55:47 -0700

    removing linkedin login for now

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -54,7 +54,7 @@ function HomeHeader() {
 
             <div className="rts__menu d-flex gap-5 align-items-center">
             <div className="header__right__btn d-flex gap-3">
-            <LinkedInPage />
+            {/* <LinkedInPage /> */}
              <button className="d-md-block d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvas" aria-controls="offcanvas"><i className="fa-sharp fa-regular fa-bars"></i></button>
 
                                 </div>
