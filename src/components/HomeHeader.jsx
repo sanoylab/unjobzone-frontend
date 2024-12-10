43bd984eff016a38884ed56e4fa1e6ca9b4977e8
yonas <expertsanoy@gmail.com>
2024-12-10 16:06:48 -0700

    add home link on the header

--- src/components/HomeHeader.jsx
+++ src/components/HomeHeader.jsx
@@ -28,7 +28,11 @@ function HomeHeader() {
             <div className="navigation d-none d-lg-block">
               <nav className="navigation__menu">
                 <ul className="list-unstyled">
-                 
+                <li className="navigation__menu--item">
+                      <Link to="/" className="navigation__menu--item__link">
+                        Home
+                      </Link>
+                    </li>
                   <li className="navigation__menu--item">
                     <Link to="/jobs" className="navigation__menu--item__link">Browse Jobs</Link>
                     
