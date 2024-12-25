729166880a69616d8f38b4a088b817fbdc4984f6
Yonas Yeneneh <expertsanoy@gmail.com>
2024-12-25 03:46:07 -0700

    fix routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -14,29 +14,21 @@ import LinkedInPage from './components/LinkedInPage';
 import LinkedInCallback from './components/LinkedInCallback';
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <>
-  <Route index element={<HomePage />} />
-  <Route path="/" element={<MainLayout />} >
-    
-    <Route path='/jobs' element={<JobsPage />} />
-    <Route path='/organizations' element={<OrganizationPage />} />
-    <Route path='/duty-stations' element={<DutyStationPage />} />
-    <Route path='/blog' element={<BlogPage />} />
-    <Route path='/job-categories' element={<JobCategoryPage />} />
-    <Route path='/job/:id' element={<JobDetail />} />
-    <Route path='/blog/:id' element={<BlogDetail />} />
-
-    <Route exact path="/linkedin" component={LinkedInCallback} />
-
-    <Route path='*' element={<NotFoundPage />} />
-
-
-
-  </Route>
-  </>
-
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="jobs" element={<JobsPage />} />
+      <Route path="organizations" element={<OrganizationPage />} />
+      <Route path="duty-stations" element={<DutyStationPage />} />
+      <Route path="blog" element={<BlogPage />} />
+      <Route path="job-categories" element={<JobCategoryPage />} />
+      <Route path="job/:id" element={<JobDetail />} />
+      <Route path="blog/:id" element={<BlogDetail />} />
+      <Route path="linkedin" element={<LinkedInCallback />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Route>
   )
 );
+
 
 const App = () => {
   return <RouterProvider router={router} />;  
