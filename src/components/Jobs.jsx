8537ccd255504603c8eed5f07c8396d1d22353da
Yonas Yeneneh <expertsanoy@gmail.com>
2024-12-25 20:56:26 -0700

    adding list and grid view to the job page

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -12,7 +12,8 @@ function Jobs({ isHome = false }) {
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const pageSize = 10; // Assuming the page size is 10
+  const [totalRecords, setTotalRecords] = useState(0);
+  const pageSize = 12; // Assuming the page size is 12
 
   const location = useLocation();
   const navigate = useNavigate();
@@ -43,10 +44,11 @@ function Jobs({ isHome = false }) {
   const fetchJobs = async (page, job_title, duty_station, dept, recruitement_type, start_date, end_date, jn, jf, jc, jl) => {
     try {
       setLoading(true);
-      const data = await getJobs(page, 10, isHome, job_title, duty_station, dept, recruitement_type, start_date, end_date, jn, jf, jc, jl);
+      const data = await getJobs(page, pageSize, isHome, job_title, duty_station, dept, recruitement_type, start_date, end_date, jn, jf, jc, jl);
+      console.log(data)
       setJobs(data.data); // Adjust according to your API response structure
-      const totalRecords = data.totalRecords;
-      setTotalPages(Math.ceil(totalRecords / pageSize));
+      setTotalRecords(data.totalRecords);
+      setTotalPages(Math.ceil(data.totalRecords / pageSize));
     } catch (error) {
       console.error("Error fetching jobs:", error);
     } finally {
@@ -61,6 +63,10 @@ function Jobs({ isHome = false }) {
       navigate(`?${queryParams.toString()}`);
     }
   };
+
+  const startRecord = (currentPage - 1) * pageSize + 1;
+  const endRecord = Math.min(currentPage * pageSize, totalRecords);
+  const formattedTotalRecords = totalRecords.toLocaleString();
 
   return isHome ? (
     <div className="rts__section pb-120 overflow-hidden">
@@ -84,12 +90,11 @@ function Jobs({ isHome = false }) {
               id={job.id}
               end_date={job.end_date}
               duty_station={job.duty_station}
-              recruitment_type={job.recruitment_type}
+              recruitment_type={job.recruitement_type}
               job_title={job.job_title}
               dept={job.dept}
               jl={job.jl}
               logo={`${APP_URL}/assets/logo/${job.logo}`}
-
             />
           ))}
         </div>
@@ -97,40 +102,99 @@ function Jobs({ isHome = false }) {
     </div>
   ) : (
     <>
-       <div className="rts__pagination d-block mx-auto pt-60 max-content">
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
-      </div>
-      <div className="row g-30" style={{paddingRight: "0px"}}>
-        {loading ? (
-          <Spinner loading={loading} />
-        ) : (
-          <>
-            {jobs.map((job) => (
-              <ListJob
-                key={job.id}
-                id={job.id}
-                end_date={job.end_date}
-                duty_station={job.duty_station}
-                recruitment_type={job.recruitment_type}
-                job_title={job.job_title}
-                dept={job.dept}
-                jl={job.jl}
-                logo={`${APP_URL}/assets/logo/${job.logo}`}
-              />
-            ))}
-          </>
-        )}
-      </div>
-      <div className="rts__pagination d-block mx-auto pt-60 max-content">
-        <Pagination
-          currentPage={currentPage}
-          totalPages={totalPages}
-          onPageChange={handlePageChange}
-        />
+      <div className="row g-30" style={{ paddingRight: "0px" }}>
+        <div className="top__query mb-40 d-flex flex-wrap gap-4 gap-xl-0 justify-content-between align-items-center">
+          <span className="text-dark font-20 fw-medium">
+          Showing {startRecord}-{endRecord} of {formattedTotalRecords} results
+          </span>
+          <div className="d-flex flex-wrap align-items-center gap-4">
+            <div className="d-flex align-items-center gap-3" id="nav-tab" role="tablist">
+              <button className="rts__btn no__fill__btn list-style nav-link active" data-bs-toggle="tab" data-bs-target="#list"> <i className="rt-list"></i> List</button>
+              <button className="rts__btn no__fill__btn grid-style nav-link" data-bs-toggle="tab" data-bs-target="#grid"> <i className="rt-hamburger"></i> Grid</button>
+            </div>
+          </div>
+        </div>
+        <div className="tab-content" id="myTabContent">
+          <div className="tab-pane grid__style fade" role="tabpanel" id="grid">
+            <div className="row g-30 mb-40">
+              <div className="rts__pagination d-block mx-auto pt-60 max-content">
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                />
+              </div>
+            </div>
+            <div className="row g-30">
+              {loading ? (
+                <Spinner loading={loading} />
+              ) : (
+                <>
+                  {jobs.map((job) => (
+                    <ThumbnailJob
+                      key={job.id}
+                      id={job.id}
+                      end_date={job.end_date}
+                      duty_station={job.duty_station}
+                      recruitment_type={job.recruitement_type}
+                      job_title={job.job_title}
+                      dept={job.dept}
+                      jl={job.jl}
+                      logo={`${APP_URL}/assets/logo/${job.logo}`}
+                    />
+                  ))}
+                </>
+              )}
+            </div>
+            <div className="row g-30">
+              <div className="rts__pagination d-block mx-auto pt-60 max-content">
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                />
+              </div>
+            </div>
+          </div>
+          <div className="tab-pane fade show active list__style" role="tabpanel" id="list">
+            <div className="row g-30">
+              <div className="rts__pagination d-block mx-auto pt-60 max-content">
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                />
+              </div>
+
+              {loading ? (
+                <Spinner loading={loading} />
+              ) : (
+                <>
+                  {jobs.map((job) => (
+                    <ListJob
+                      key={job.id}
+                      id={job.id}
+                      end_date={job.end_date}
+                      duty_station={job.duty_station}
+                      recruitment_type={job.recruitement_type}
+                      job_title={job.job_title}
+                      dept={job.dept}
+                      jl={job.jl}
+                      logo={`${APP_URL}/assets/logo/${job.logo}`}
+                    />
+                  ))}
+                </>
+              )}
+              <div className="rts__pagination d-block mx-auto pt-60 max-content">
+                <Pagination
+                  currentPage={currentPage}
+                  totalPages={totalPages}
+                  onPageChange={handlePageChange}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
